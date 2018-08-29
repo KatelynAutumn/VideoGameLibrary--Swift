8cfcbbf2d9fde4d4121058e947e7d4e298be8c69
Katelyn Pace <katelynpace@interapts-macbook-pro-4.local>
2018-08-29 12:59:51 -0500

    Initial Commit

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Katelyn Pace on 8/29/18.
+//  Copyright © 2018 Katelyn Pace. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
